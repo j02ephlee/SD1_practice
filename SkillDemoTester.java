1fa92070b34ed0b19827eb748a004ba19bed4a23
j02ephlee <jul050@ucsd.edu>
2022-05-01 20:20:58 -0700

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -1,12 +1,9 @@
 import static org.junit.Assert.*;
-
-import java.beans.Transient;
-
 import org.junit.*;
 
 public class SkillDemoTester {
     @Test
     public void TwoPlusTwoShouldEqualFour() {
-        assertEquals(4, add(2, 2));
+        assertEquals(4, SkillDemo.add(2, 2));
     }
 }
